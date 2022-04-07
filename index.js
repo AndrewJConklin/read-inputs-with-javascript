1626f99e4ba04652f1ffd29c116611d2d4923728
aconkli9 <aconkli9@ford.com>
2022-04-07 13:01:08 -0400

    Add solution

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-function logValues(){
-  // Your code here
+function logValues() {
+  const form = document.querySelector("form")
+  form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const formData = new FormData(event.target)
+    const firstName = formData.get("first-name")
+    const lastname = formData.get("last-name")
+    const email = formData.get("email")
+    console.log(firstName, lastname, email)
+  })
 }
